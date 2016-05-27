97b7ac1a2828623c5c1bc90a8306dec0d25015f4
LMErika <erikiita_18@hotmail.com>
2016-05-26 23:31:05 -0500

    Clase usuario

--- SisAlmacen/CapaEntidades/entUsuario.cs
+++ SisAlmacen/CapaEntidades/entUsuario.cs
@@ -8,5 +8,16 @@ namespace CapaEntidades
 {
     public class entUsuario
     {
+        public int idUsuario { get; set; }
+        public String Nombre { get; set; }
+        public String ApellidoPaterno { get; set; }
+        public String ApellidoMaterno { get; set; }
+        public String dni { get; set; }
+        public String Usuario { get; set; }
+        public String Contasena { get; set; }
+        public DateTime Fechanacimiento { get; set; }
+        public String Direccion { get; set; }
+        public String Email { get; set; }
+        public String fotografia { get; set; }
     }
 }
