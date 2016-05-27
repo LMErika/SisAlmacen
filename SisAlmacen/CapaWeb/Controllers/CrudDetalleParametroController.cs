53e0cb4cf93ff7dda00fdaf3466f7b26d3f2ef08
EMarquez394 <eduard_394@hotmail.com>
2016-05-27 03:08:19 -0500

    Controladores de mantenedores y Vistas

---
+++ SisAlmacen/CapaWeb/Controllers/CrudDetalleParametroController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CapaWeb.Controllers
+{
+    public class CrudDetalleParametroController : Controller
+    {
+        public ActionResult ListarDetalleParametro()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SisAlmacen/CapaWeb/Controllers/CrudDistribuidorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CapaWeb.Controllers
+{
+    public class CrudDistribuidorController : Controller
+    {
+        //
+        // GET: /CrudDistribuidor/
+
+        public ActionResult ListadoDistribuidor()
+        {
+            return View();
+        }
+
+    }
+}
