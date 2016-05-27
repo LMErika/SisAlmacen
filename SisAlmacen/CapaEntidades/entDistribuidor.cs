297ebbf59be5d3bf525e8867ce6577ed58eb6bf4
LMErika <erikiita_18@hotmail.com>
2016-05-27 02:43:24 -0500

    Capa Entidades Distribuidor Completo

---
+++ SisAlmacen/CapaEntidades/entDistribuidor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class entDistribuidor
+    {
+        public int idDistribuidor { get; set; }
+        public String RazonSocial { get; set; }
+        public String Ruc { get; set; }
+        public String Direccion { get; set; }
+        public String Telefono { get; set; }
+        public String Email { get; set; }
+        public Boolean estado { get; set; }
+
+    }
+}
