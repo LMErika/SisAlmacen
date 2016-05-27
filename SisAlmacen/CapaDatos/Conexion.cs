fb3d10d1ed50496ca4a54f738909b08045c6506d
LMErika <erikiita_18@hotmail.com>
2016-05-26 23:16:15 -0500

    Acabado Clase Conexion

--- SisAlmacen/CapaDatos/Conexion.cs
+++ SisAlmacen/CapaDatos/Conexion.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace CapaDatos
 {
     public class Conexion
     {
+        #region singleton
+        private static readonly Conexion _instancia = new Conexion();
+        public static Conexion Instancia
+        {
+            get { return Conexion._instancia; }
+        }
+        #endregion singleton
+
+        #region metodos
+        public SqlConnection conectar()
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Data source=localhost;Initial Catalog=SisAlmacen;" +
+                //"User ID=sa;Password=123"
+                                   "Integrated Security=true";
+            return cn;
+        }
+        #endregion metodos
     }
 }
