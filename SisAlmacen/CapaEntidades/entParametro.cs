a798963f44d98f89f3413546c7e60663baab3cba
LMErika <erikiita_18@hotmail.com>
2016-05-26 23:44:14 -0500

    clase entParametro completo

---
+++ SisAlmacen/CapaEntidades/entParametro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class entParametro
+    {
+        public int idParametro { get; set; }
+        public String Codigo { get; set; }
+        public String Descripcion { get; set; }
+    }
+}
