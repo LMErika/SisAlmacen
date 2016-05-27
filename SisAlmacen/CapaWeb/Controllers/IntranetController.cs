1a47dc570b6cceb8a391e40b0bf0f90c4ea9e47f
EMarquez394 <eduard_394@hotmail.com>
2016-05-26 23:51:51 -0500

    CapaWeb

---
+++ SisAlmacen/CapaWeb/Controllers/IntranetController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CapaEntidades;
+using CapaLogica;
+
+namespace CapaWeb.Controllers
+{
+    public class IntranetController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(FormCollection fmr)
+        {
+            try
+            {
+                entUsuario u = LogUsuario.Instancia.VerificarAcceso(fmr["txtUsuario"].ToString(),
+                                                                   fmr["txtContasena"].ToString());
+                if (u != null)
+                {
+                    Session["usuarito"] = u;
+                    return RedirectToAction("Principal", "Intranet");
+                }
+                else
+                {
+                    ViewBag.mensaje = "Usuario o Contraseña no Valido";
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+        public ActionResult Principal()
+        {
+            return View();
+
+        }
+    }
+}
